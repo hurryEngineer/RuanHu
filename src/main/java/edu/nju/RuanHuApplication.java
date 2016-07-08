c0e02329a3f460591dd3e61d49f9046e265e74d7
彬 申 <705103197@qq.com>
2016-07-08 15:58:57 +0800

    第一次提交

---
+++ src/main/java/edu/nju/RuanHuApplication.java
@@ -0,0 +1,12 @@
+package edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuanHuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuanHuApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/nju/RuanHuApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.nju;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RuanHuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
