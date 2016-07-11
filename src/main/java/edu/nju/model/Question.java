4828ccba4fd6b99a1e7014b976db95067986edb6
oraisdy <569450503@qq.com>
2016-07-11 12:10:33 +0800

    just copy

---
+++ src/main/java/edu/nju/model/Question.java
@@ -0,0 +1,7 @@
+package edu.nju.model;
+
+/**
+ * Created by Dora on 2016/7/11.
+ */
+public class Question {
+}
---
+++ src/main/java/edu/nju/webConfig/ChartsetConfig.java
@@ -0,0 +1,19 @@
+package edu.nju.webConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class ChartsetConfig {
+    
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter =new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+     }
+}
