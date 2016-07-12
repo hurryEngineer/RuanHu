f2ad454eec19cb644868344bf4e31d4cf0647111
cuiods <cuihaoemail@163.com>
2016-07-12 14:19:19 +0800

    Development: bug

--- src/main/java/edu/nju/logic/impl/LoginImpl.java
+++ src/main/java/edu/nju/logic/impl/LoginImpl.java
@@ -1,37 +1,30 @@
 package edu.nju.logic.impl;
 
-<<<<<<< HEAD
-=======
 import edu.nju.data.dao.UserDAO;
-import edu.nju.data.entity.Users;
+import edu.nju.data.entity.User;
 import edu.nju.data.util.VerifyResult;
->>>>>>> f5ef5ae24a066fa81604ccef4f4329708f7f0b77
 import edu.nju.logic.service.LoginService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * 登陆实现
  * @author cuihao
  * @see edu.nju.logic.service.LoginService
  */
-@Component
-public class LoginImp implements LoginService{
+@Repository
+public class LoginImpl implements LoginService{
     @Autowired
     private UserDAO userDAO;
     @Override
-<<<<<<< HEAD
-    public User verifyLogin(String account, String password) {
-        return null;
-=======
     public VerifyResult verifyLogin(String account, String password) {
         return userDAO.login(account,password);
     }
 
     @Override
-    public Users getCurrentUser(String account) {
+    public User getCurrentUser(String account) {
         return userDAO.getUserByName(account);
->>>>>>> f5ef5ae24a066fa81604ccef4f4329708f7f0b77
     }
 }
