f1fd5e0138a4be9f145428e9b7e5521ca996bb12
njushishuo <675342907@qq.com>
2016-07-20 15:09:23 +0800

    d

--- src/main/java/edu/nju/data/daoImpl/WikiDAOImpl.java
+++ src/main/java/edu/nju/data/daoImpl/WikiDAOImpl.java
@@ -8,7 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by ss14 on 2016/7/20.
@@ -25,7 +28,7 @@ public class WikiDAOImpl implements WikiDAO {
     @Override
     public List getRelatedQuestions(long wikiID) {
 
-        Query query = em.createQuery("select questionID from QuestionWiki  where wikiID = ?1");
+        Query query = em.createQuery("select qw.questionId from QuestionWiki qw  where qw.wikiId = ?1");
         query.setParameter(1,wikiID);
         List result = query.getResultList();
         return result;
@@ -34,16 +37,22 @@ public class WikiDAOImpl implements WikiDAO {
     @Override
     public List getRelatedDocuments(long wikiID) {
 
-        Query query = em.createQuery("select documentID from QuestionWiki join QuestionDocument where wikiID = ?1");
+        Query query = em.createQuery("select qd.documentId from QuestionWiki qw, QuestionDocument qd " +
+                "where qw.questionId = qd.questionId and qw.wikiId = ?1");
         query.setParameter(1,wikiID);
         List resultQuestion = query.getResultList();
 
-        query = em.createQuery("select documentID from AnswerWiki join AnswerDocument where wikiID = ?1");
+        query = em.createQuery("select ad.documentId from AnswerWiki aw , AnswerDocument ad  " +
+                "where aw.answerId = ad.answerId and aw.wikiId = ?1");
         query.setParameter(1,wikiID);
         List resultAnswer = query.getResultList();
 
-        resultQuestion.addAll(resultAnswer);
+        Set resultSet = new HashSet<>();
+        resultSet.addAll(resultQuestion);
+        resultSet.addAll(resultAnswer);
 
-        return resultQuestion;
+        List resultList = new ArrayList(resultSet);
+
+        return resultList;
     }
 }
