002f49fafee5da0428d0cd4426f72d0d7f15a0fb
cuiods <cuihaoemail@163.com>
2016-07-20 17:37:40 +0800

    急着要

--- src/main/java/edu/nju/web/controller/WikiController.java
+++ src/main/java/edu/nju/web/controller/WikiController.java
@@ -1,9 +1,12 @@
 package edu.nju.web.controller;
 
+import edu.nju.data.dao.http.Wiki_httpDAO;
 import edu.nju.data.entity.api.WikiItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -12,19 +15,29 @@ import java.util.List;
 @Controller
 @RequestMapping("/api")
 public class WikiController {
+
+    @Autowired
+    private Wiki_httpDAO wiki_httpDAO;
+
     @RequestMapping("/entry/{id}")
     @ResponseBody
     public WikiItem getWikiById(@PathVariable long id) {
-        return null;
+        WikiItem wikiItem = new WikiItem();
+        try {
+            wikiItem = wiki_httpDAO.getWikiById(id);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wikiItem;
     }
 
-    @RequestMapping("/api/search")
+    @RequestMapping("/search")
     @ResponseBody
     public List<WikiItem> searchWikis(@RequestParam("key")String key) {
         return null;
     }
 
-    @RequestMapping(value = "/api/keymacth", method = RequestMethod.POST)
+    @RequestMapping(value = "/keymacth", method = RequestMethod.POST)
     @ResponseBody
     public String keyMatch(@RequestParam("content")String content) {
         return null;
